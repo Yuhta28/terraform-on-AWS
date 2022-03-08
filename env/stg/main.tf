0649b80283f1c2d27c6ec9c0edcc33ee3354a38a
Yuta <yuta_mori@outlook.jp>
2022-03-08 22:29:17 +0900

    fix backend

--- env/stg/main.tf
+++ env/stg/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-s3-yuta1993"
-    region = "ap-northeast-1"
-    key    = "stg/terraform.tfstate"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-s3-yuta1993"
+#    region = "ap-northeast-1"
+#    key    = "stg/terraform.tfstate"
+#  }
+#}
