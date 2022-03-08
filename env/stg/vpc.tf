56b7a0bf2bec116b42429f56470e2ca9db0c2534
Yuta <yuta_mori@outlook.jp>
2022-03-08 22:42:26 +0900

    fix

--- env/stg/vpc.tf
+++ env/stg/vpc.tf
@@ -4,5 +4,5 @@ module "staging-vpc" {
   Tag_Name   = "staging"
   public-AZ  = { a = "192.168.0.0/20", c = "192.168.16.0/20" }
   private-AZ = { a = "192.168.128.0/20", c = "192.168.144.0/20" }
-  #eip-NAT-AZ = ["a"]
+  eip-NAT-AZ = ["a"]
 }
