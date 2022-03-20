d39279760ff9c1899047f61511d5a74ba9c1c50e
Yuta <yuta_mori@outlook.jp>
2022-03-20 14:31:21 +0900

    fix subnet-id

--- env/stg/ec2.tf
+++ env/stg/ec2.tf
@@ -3,5 +3,5 @@ module "staging-ec2" {
   ec2_instance_type = "t2.micro"
   ami               = "ami-04204a8960917fd92"
   Tag_Name          = "staging"
-  terraform-public-subnet-id = [ module.staging-vpc.terraform-public-subnet-id ]
+  terraform-public-subnet-id = [module.staging-vpc.terraform-public-subnet-id]
 }
