f462e4ab64284c42678100712074be4e6954311e
Yuta <yuta_mori@outlook.jp>
2022-03-20 13:49:10 +0900

    fix-subnet

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "terraform-ec2" {
   ami           = var.ami
   instance_type = var.ec2_instance_type
+  subnet_id = [ aws_subnet.terraform-public-subnet[0].id ]
   tags = {
     Name      = "${var.Tag_Name}-ec2"
     Terraform = "True"
