241aaad8f8c596056561dcdd3ddbe01f0f0ad52d
Yuta <yuta_mori@outlook.jp>
2022-03-20 16:33:15 +0900

    fix change

--- env/stg/ec2.tf
+++ env/stg/ec2.tf
@@ -4,7 +4,6 @@ module "staging-ec2" {
   ami                        = "ami-04204a8960917fd92"
   Tag_Name                   = "staging"
   key_name                   = "WindowsKey"
-  EC2-SG                     = ["sg-016e828da37879979"]
   terraform-public-subnet-id = [module.staging-vpc.terraform-public-subnet-id]
   terraform-vpc-id           = module.staging-vpc.terraform-vpc-id
 }
