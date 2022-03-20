ad2d681000755bf6090900ffed4fa222a5e6a9ee
Yuta <yuta_mori@outlook.jp>
2022-03-20 13:24:56 +0900

    delete prd and fix stg

--- env/stg/ec2.tf
+++ env/stg/ec2.tf
@@ -1,4 +1,8 @@
 module "staging-ec2" {
-    source = "../../modules/ec2"
-  
+  source            = "../../modules/ec2"
+  ec2_instance_type = "t2.micro"
+  ami               = "ami-04204a8960917fd92"
+  Tag_Name          = "staging"
+
+
 }
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "ec2_instance_type" {
+  type        = string
+  description = "Instance type"
+}
+
+variable "ami" {
+  type        = string
+  description = "ami id"
+}
+
+variable "Tag_Name" {
+  type        = string
+  description = "Tag Name"
+}
