cfd425b72a43dc40bac3a6ac6d93481aab1477f8
Yuta <yuta_mori@outlook.jp>
2022-03-21 14:55:47 +0900

    change vpc output

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -83,5 +83,6 @@ resource "aws_lb" "terraform-alb" {
   security_groups = [
     aws_security_group.web_server_sg.id
   ]
-  subnets =  [for subnet in var.terraform-public-subnet-id : subnet.id]
+#  subnets =  [for subnet in var.terraform-public-subnet-id : subnet.id]
+  subnets =  ["subnet-046b28c6889f479b9", "subnet-010adb826637c2663"]
 }
