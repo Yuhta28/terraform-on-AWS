f0662bf323e07eb3b598f146244694cd7517f5cd
Yuta <yuta_mori@outlook.jp>
2022-03-21 11:59:50 +0900

    add data

---
+++ modules/ec2/data.tf
@@ -0,0 +1,15 @@
+data "aws_suaws_subnets" "terraform-subnets" {
+    filter {
+    name   = "vpc-id"
+    values = [var.terraform-vpc-id]
+  }
+}
+
+data "aws_subnet" "terraform-subnet" {
+  for_each = toset(data.aws_subnets.terraform-subnets.ids)
+  id       = each.value
+}
+
+output "terraform-subnet-ids" {
+  value = toset(data.aws_subnet.terraform-subnet.ids)
+}
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,17 @@
-data "aws_subnet_ids" "terraform-public-subnet-id" {
-  vpc_id = var.terraform-vpc-id
+data "aws_suaws_subnets" "terraform-subnets" {
     filter {
-    name   = "tag:Name"
-    values = ["public"]
+    name   = "vpc-id"
+    values = [var.terraform-vpc-id]
   }
+}
+
+data "aws_subnet" "terraform-subnet" {
+  for_each = toset(data.aws_subnets.terraform-subnets.ids)
+  id       = each.value
+}
+
+output "terraform-subnet-ids" {
+  value = toset(data.aws_subnet.terraform-subnet.ids)
 }
 
 resource "aws_instance" "terraform-ec2" {
