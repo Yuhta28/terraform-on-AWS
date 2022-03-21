00c8e00e707e3c700a8b60054ee45c45c5a4f280
Yuta <yuta_mori@outlook.jp>
2022-03-21 13:07:45 +0900

    add data

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,19 +1,3 @@
-data "aws_suaws_subnets" "terraform-subnets" {
-    filter {
-    name   = "vpc-id"
-    values = [var.terraform-vpc-id]
-  }
-}
-
-data "aws_subnet" "terraform-subnet" {
-  for_each = toset(data.aws_subnets.terraform-subnets.ids)
-  id       = each.value
-}
-
-output "terraform-subnet-ids" {
-  value = toset(data.aws_subnet.terraform-subnet.ids)
-}
-
 resource "aws_instance" "terraform-ec2" {
   ami                         = var.ami
   instance_type               = var.ec2_instance_type
