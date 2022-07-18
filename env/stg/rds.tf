95db455ca5ab3ba1af872d03efa8d70b2741a078
Yuta <yuta_mori@outlook.jp>
2022-07-18 22:07:09 +0900

    change env

--- env/stg/rds.tf
+++ env/stg/rds.tf
@@ -2,5 +2,5 @@ module "staging-rds" {
   source              = "../../modules/rds"
   db_sunet_group_name = "terraform-staging-db-subent"
 #  db_sunet_group_ids  = [ module.staging-vpc.terraform-private-subnet-a.id, module.staging-vpc.terraform-private-subent-c.id ]
-  db_sunet_group_ids  = [ module.staging-vpc.terraform-private-subnet-a.id]
+  db_sunet_group_ids  = [ module.staging-vpc.terraform-private-subnet-a.id, module.staging-vpc.terraform-private-subnet-c.id]
 }
