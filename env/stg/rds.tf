dbc7e054daeaea9a79a3fc4c268472accfff5dce
Yuta <yuta_mori@outlook.jp>
2022-07-18 21:59:52 +0900

    change env

--- env/stg/rds.tf
+++ env/stg/rds.tf
@@ -1,5 +1,5 @@
 module "staging-rds" {
   source              = "../../modules/rds"
   db_sunet_group_name = "terraform-staging-db-subent"
-  db_sunet_group_ids  = module.staging-vpc.terraform-private-subent-id
+  db_sunet_group_ids  = [ module.staging-vpc.terraform-private-subent-a.id, module.staging-vpc.terraform-private-subent-c.id ]
 }
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "terraform-db-subnet" {
   name       = var.db_sunet_group_name
-  subnet_ids = [var.db_sunet_group_ids]
+  subnet_ids = var.db_sunet_group_ids
 
   tags = {
     Name      = var.db_sunet_group_name
