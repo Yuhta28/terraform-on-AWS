f2f82815211eea4b0e42240a79e09a20c22fbfb7
Yuta <yuta_mori@outlook.jp>
2022-07-23 21:43:51 +0900

    add second EC2

--- env/stg/ec2.tf
+++ env/stg/ec2.tf
@@ -1,7 +1,7 @@
 module "staging-ec2" {
   source                     = "../../modules/ec2"
   ec2_instance_type          = "t3.small"
-  ami                        = "ami-0b7546e839d7ace12"
+  ami                        = "ami-09b4241c3ccbe9b01"
   Tag_Name                   = "staging"
   key_name                   = "WindowsKey"
   terraform-public-subnet-id = [module.staging-vpc.terraform-public-subnet-id][0]
