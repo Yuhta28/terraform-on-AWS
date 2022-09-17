5c7ce304e8302bb8aba008147a4313a1f155b442
Yuta <yuta_mori@outlook.jp>
2022-09-17 11:25:07 +0900

    fix

--- env/stg/ec2.tf
+++ env/stg/ec2.tf
@@ -1,9 +1,9 @@
-#module "staging-ec2" {
-#  source                     = "../../modules/ec2"
-#  ec2_instance_type          = "t3.small"
-#  ami                        = "ami-09b4241c3ccbe9b01"
-#  Tag_Name                   = "staging"
-#  key_name                   = "WindowsKey"
-#  terraform-public-subnet-id = [module.staging-vpc.terraform-public-subnet-id][0]
-#  terraform-vpc-id           = module.staging-vpc.terraform-vpc.id
-#}
+module "staging-ec2" {
+  source                     = "../../modules/ec2"
+  ec2_instance_type          = "t3.small"
+  ami                        = "ami-09b4241c3ccbe9b01"
+  Tag_Name                   = "staging"
+  key_name                   = "WindowsKey"
+  terraform-public-subnet-id = [module.staging-vpc.terraform-public-subnet-id][0]
+  terraform-vpc-id           = module.staging-vpc.terraform-vpc.id
+}
