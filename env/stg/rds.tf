414d444996f5b8a49f08b5a64121162ae7e1f7e0
Yuta <yuta_mori@outlook.jp>
2022-09-17 14:32:57 +0900

    fix DB sg

--- env/stg/rds.tf
+++ env/stg/rds.tf
@@ -1,7 +1,7 @@
 module "staging-rds" {
   source                               = "../../modules/rds"
   terraform-vpc-id                     = module.staging-vpc.terraform-vpc.id
-  ec2_to_db_security_groups_id         = [module.staging-ec2.sg-for-alb-to-ec2.id]
+  ec2_to_db_security_groups_id         = [module.staging-ec2.terraform-sg-attached-ap.id]
   db_subnet_group_name                 = "terraform-staging-db-subent"
   db_subnet_group_description          = "DB Subnet Group for staging"
   db_subnet_group_ids                  = [module.staging-vpc.terraform-private-subnet-a.id, module.staging-vpc.terraform-private-subnet-c.id]
