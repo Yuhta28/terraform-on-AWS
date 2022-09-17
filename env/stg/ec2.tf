9e08eaccd86a5e26c30d1a0362c58bb4ce8114da
Yuta <yuta_mori@outlook.jp>
2022-09-17 13:58:20 +0900

    fix subnet

--- env/stg/ec2.tf
+++ env/stg/ec2.tf
@@ -4,6 +4,6 @@ module "staging-ec2" {
   ap_ami_name                = "ami-09b4241c3ccbe9b01"
   Tag_Name                   = "staging"
   terraform-public-subnet-id = [module.staging-vpc.terraform-public-subnet-id][0]
-  terraform-private-subnet-id = [module.staging-vpc.terraform-private-subnet-a.id, module.staging-vpc.terraform-private-subnet-c.id, module.staging-vpc.terraform-private-subnet-d.id]
+  terraform-private-subnet-id = [module.staging-vpc.terraform-private-subnet-a.id, module.staging-vpc.terraform-private-subnet-c.id,]
   terraform-vpc-id           = module.staging-vpc.terraform-vpc.id
 }
