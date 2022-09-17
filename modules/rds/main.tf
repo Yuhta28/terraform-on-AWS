38e0348afe4f9f9692bc04ac141e382cf22c2375
Yuta <yuta_mori@outlook.jp>
2022-09-17 11:22:51 +0900

    AWS init

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -4,10 +4,11 @@ resource "aws_db_subnet_group" "terraform-db-subnet" {
   description = var.db_subnet_group_description
 
   tags = {
-    Name      = var.db_subnet_group_name
+    Name = var.db_subnet_group_name
   }
 }
 
+# EC2からの通信を許可するためのRDS用のセキュリティグループ
 resource "aws_security_group" "terraform-ec2-to-db" {
   name        = "${var.Tag_Name}-ec2-to-db"
   description = "Security group for ${var.Tag_Name}-db connected ec2"
@@ -27,7 +28,7 @@ resource "aws_security_group" "terraform-ec2-to-db" {
     ipv6_cidr_blocks = ["::/0"]
   }
   tags = {
-    Name      = "${var.Tag_Name}-ec2-to-db"
+    Name = "${var.Tag_Name}-ec2-to-db"
   }
 }
 
@@ -36,26 +37,67 @@ resource "aws_rds_cluster_parameter_group" "terraform-aurora-cluster-parameter" 
   family      = "aurora-mysql5.7"
   description = "RDS terraform aurora cluster parameter group"
 }
-
 resource "aws_db_parameter_group" "terraform-aurora-instance-parameter-group" {
   name        = var.aurora_instance_parameter_group_name
   family      = "aurora-mysql5.7"
   description = "RDS terraform aurora instance parameter group"
 }
 
-resource "aws_rds_cluster" "terraform-aurora-cluster" {
-  cluster_identifier = var.db_cluster_name
-  engine = "aurora-mysql"
-  db_subnet_group_name = aws_db_subnet_group.terraform-db-subnet.name
-  snapshot_identifier = "terraform-staging-snapshot"
-  skip_final_snapshot  = true
-  vpc_security_group_ids = [aws_security_group.terraform-ec2-to-db.id]
+# RDSの暗号化キーをマネージドキーから取得する
+data "aws_kms_key" "rds" {
+  key_id = "alias/aws/rds"
 }
 
-resource "aws_rds_cluster_instance" "terraform-aurora-cluster-instance" {
-  identifier = "${var.db_cluster_name}-instance"
-  cluster_identifier = aws_rds_cluster.terraform-aurora-cluster.id
-  instance_class = var.db_cluster_instance
-  db_subnet_group_name = aws_db_subnet_group.terraform-db-subnet.name
-  engine = aws_rds_cluster.terraform-aurora-cluster.engine
+resource "aws_rds_cluster" "terraform-aurora-cluster" {
+  cluster_identifier     = var.db_cluster_name
+  engine                 = "aurora-mysql"
+  db_subnet_group_name   = aws_db_subnet_group.terraform-db-subnet.name
+  snapshot_identifier    = "terraform-staging-snapshot"
+  skip_final_snapshot    = true
+  engine_mode            = "provisioned"
+  vpc_security_group_ids = [aws_security_group.terraform-ec2-to-db.id]
+  kms_key_id = data.aws_kms_key.rds.arn
+  enabled_cloudwatch_logs_exports = [
+    "error",
+    "general",
+    "slowquery"
+  ]
 }
+resource "aws_rds_cluster_instance" "terraform-aurora-cluster-instance" {
+  identifier           = "${var.db_cluster_name}-instance"
+  cluster_identifier   = aws_rds_cluster.terraform-aurora-cluster.id
+  instance_class       = var.db_cluster_instance
+  db_subnet_group_name = aws_db_subnet_group.terraform-db-subnet.name
+  engine               = aws_rds_cluster.terraform-aurora-cluster.engine
+  storage_encrypted    = true
+  monitoring_interval  = 60
+  monitoring_role_arn  = aws_iam_role.terraform-iam-role-rds-monitoring.arn
+}
+
+##########################################################################
+# RDSがCloudWatchへログを送るためのIAMロールを付与
+data "aws_iam_policy" "AmazonRDSEnhancedMonitoringRole" {
+  name = "AmazonRDSEnhancedMonitoringRole"
+}
+resource "aws_iam_role" "terraform-iam-role-rds-monitoring" {
+  name = "${var.Tag_Name}-rds-monitoring"
+  path = "/"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "monitoring.rds.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "terraform-iam-role-attachment-rds-monitoring" {
+  role       = aws_iam_role.terraform-iam-role-rds-monitoring.name
+  policy_arn = data.aws_iam_policy.AmazonRDSEnhancedMonitoringRole.arn
+}
+##########################################################################
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,16 +2,19 @@ resource "aws_vpc" "terraform-vpc" {
   enable_dns_hostnames = true
   cidr_block           = var.cidr_block
   tags = {
-    Name      = "${var.Tag_Name}-vpc"
+    Name = "${var.Tag_Name}-vpc"
   }
 }
 
 resource "aws_internet_gateway" "terraform-igw" {
   vpc_id = aws_vpc.terraform-vpc.id
   tags = {
-    Name      = "${var.Tag_Name}-igw"
+    Name = "${var.Tag_Name}-igw"
   }
 }
+
+##########################################################################
+######## パブリックサブネットリソース
 
 resource "aws_subnet" "terraform-public-subnet" {
   for_each                = var.public-AZ
@@ -20,43 +23,9 @@ resource "aws_subnet" "terraform-public-subnet" {
   availability_zone       = "ap-northeast-1${each.key}"
   map_public_ip_on_launch = true
   tags = {
-    Name      = "terraform-${var.Tag_Name}-public-subnet-${each.key}"
+    Name = "terraform-${var.Tag_Name}-public-subnet-${each.key}"
   }
 }
-
-resource "aws_subnet" "terraform-private-subnet" {
-  for_each          = var.private-AZ
-  vpc_id            = aws_vpc.terraform-vpc.id
-  cidr_block        = each.value
-  availability_zone = "ap-northeast-1${each.key}"
-  tags = {
-    Name      = "terraform-${var.Tag_Name}-private-subnet-${each.key}"
-  }
-}
-
-#resource "aws_eip" "terraform-nat-eip" {
-#  for_each = toset(var.eip-NAT-AZ)
-#  tags = {
-#    Name = "${var.Tag_Name}-eip-${each.key}"
-#  }
-#  depends_on = [
-#    aws_internet_gateway.terraform-igw
-#  ]
-#}
-#
-#resource "aws_nat_gateway" "terraform-nat" {
-#  for_each  = toset(var.eip-NAT-AZ)
-#  subnet_id = aws_subnet.terraform-public-subnet[each.key].id
-#  depends_on = [
-#    aws_internet_gateway.terraform-igw
-#  ]
-#  allocation_id = aws_eip.terraform-nat-eip[each.key].id
-#  tags = {
-#    Name      = "${var.Tag_Name}-nat-${each.key}"
-#    Terraform = "True"
-#  }
-#}
-#
 resource "aws_route_table" "terraform-public-rt" {
   for_each = var.public-AZ
   vpc_id   = aws_vpc.terraform-vpc.id
@@ -65,30 +34,65 @@ resource "aws_route_table" "terraform-public-rt" {
     gateway_id = aws_internet_gateway.terraform-igw.id
   }
   tags = {
-    Name      = "${var.Tag_Name}-public-rt-${each.key}"
+    Name = "${var.Tag_Name}-public-rt-${each.key}"
   }
 }
-
 resource "aws_route_table_association" "terraform-public-rt-assoc" {
   for_each       = var.public-AZ
   subnet_id      = aws_subnet.terraform-public-subnet[each.key].id
   route_table_id = aws_route_table.terraform-public-rt[each.key].id
 }
 
+# NATゲートウェイリソース
+resource "aws_nat_gateway" "terraform-nat" {
+  for_each  = toset(var.eip-NAT-AZ)
+  subnet_id = aws_subnet.terraform-public-subnet[each.key].id
+  depends_on = [
+    aws_internet_gateway.terraform-igw
+  ]
+  allocation_id = aws_eip.terraform-nat-eip[each.key].id
+  tags = {
+    Name      = "${var.Tag_Name}-nat-${each.key}"
+    Terraform = "True"
+  }
+}
+resource "aws_eip" "terraform-nat-eip" {
+  for_each = toset(var.eip-NAT-AZ)
+  tags = {
+    Name = "${var.Tag_Name}-eip-${each.key}"
+  }
+  depends_on = [
+    aws_internet_gateway.terraform-igw
+  ]
+}
+##########################################################################
+
+##########################################################################
+######## プライベートサブネットリソース
+
+resource "aws_subnet" "terraform-private-subnet" {
+  for_each          = var.private-AZ
+  vpc_id            = aws_vpc.terraform-vpc.id
+  cidr_block        = each.value
+  availability_zone = "ap-northeast-1${each.key}"
+  tags = {
+    Name = "terraform-${var.Tag_Name}-private-subnet-${each.key}"
+  }
+}
 resource "aws_route_table" "terraform-private-rt" {
   for_each = var.private-AZ
   vpc_id   = aws_vpc.terraform-vpc.id
-  #  route {
-  #    cidr_block = "0.0.0.0/0"
-  #    nat_gateway_id = aws_nat_gateway.terraform-nat.id
-  #  }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = var.Tag_Name == "staging" ? aws_nat_gateway.terraform-nat["a"].id : aws_nat_gateway.terraform-nat[each.key].id
+  }
   tags = {
-    Name      = "${var.Tag_Name}-private-rt-${each.key}"
+    Name = "${var.Tag_Name}-private-rt-${each.key}"
   }
 }
-
 resource "aws_route_table_association" "terraform-private-rt-assoc" {
   for_each       = var.public-AZ
   subnet_id      = aws_subnet.terraform-private-subnet[each.key].id
   route_table_id = aws_route_table.terraform-private-rt[each.key].id
 }
+##########################################################################
