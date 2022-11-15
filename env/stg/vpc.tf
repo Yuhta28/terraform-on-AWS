0c20ad0a156d4c18c86f42f8175e92b15260ca6f
Yuta <yuta_mori@outlook.jp>
2022-11-15 19:54:21 +0900

    init

--- env/stg/vpc.tf
+++ env/stg/vpc.tf
@@ -1,8 +1,8 @@
-#module "staging-vpc" {
-#  source     = "../../modules/vpc"
-#  cidr_block = "192.168.0.0/16"
-#  Tag_Name   = "staging"
-#  public-AZ  = { a = "192.168.0.0/20", c = "192.168.16.0/20" }
-#  private-AZ = { a = "192.168.128.0/20", c = "192.168.144.0/20" }
-#  eip-NAT-AZ = ["a"]
-#}
+module "staging-vpc" {
+  source     = "../../modules/vpc"
+  cidr_block = "192.168.0.0/16"
+  Tag_Name   = "staging"
+  public-AZ  = { a = "192.168.0.0/20", c = "192.168.16.0/20" }
+  private-AZ = { a = "192.168.128.0/20", c = "192.168.144.0/20" }
+  eip-NAT-AZ = ["a"]
+}
